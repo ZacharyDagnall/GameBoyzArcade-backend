b846c863214d3ad42fb7fd45bc832f06857491b9
Patrick Tuszakowski <mrtall500@gmail.com>
2021-03-09 11:14:59 -0500

    more default stuff

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,14 @@ class Game < ApplicationRecord
   belongs_to :user
   
   def initialize(attributes=nil)
-    attr_with_defaults = {board_state: [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]], game_over: false, score: 0}.merge(attributes)
+    if attributes[:name] == "soff" 
+      attr_with_defaults = {board_state: [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]], game_over: false, score: 0}.merge(attributes)
+      super(attr_with_defaults)
+
+    else attributes[:name] == "tictactoe" 
+      attr_with_defaults = {board_state: [[0,0,0],[0,0,0],[0,0,0]], game_over: false, score: 0}.merge(attributes)
     super(attr_with_defaults)
+    end
   end
 
 end
