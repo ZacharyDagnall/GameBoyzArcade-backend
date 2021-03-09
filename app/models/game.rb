2b38385cd803f8371dad44d2ed8833217158518a
Patrick Tuszakowski <mrtall500@gmail.com>
2021-03-09 09:27:38 -0500

    set defaults

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ApplicationRecord
   belongs_to :user
+  
+  def initialize(attributes=nil)
+    attr_with_defaults = {board_state: [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]], game_over: false, score: 0}.merge(attributes)
+    super(attr_with_defaults)
+  end
+
 end
