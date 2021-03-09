83ade181a8912c88eae8a9c198bb7dfe88bd04d3
Patrick Tuszakowski <mrtall500@gmail.com>
2021-03-09 13:51:30 -0500

    User Route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def create
+        @user = User.create(user_params) 
+    end
+
+
+
+    private 
+    
+    def user_params
+        params.permit(:name)
+    end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "/games/:id", to: "games#show"
   patch "/games/:id", to: "games#update"
 
+  get "/users" to: "user#index"
+  post "/users" to: "user#create"
+
   # resources :games
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
