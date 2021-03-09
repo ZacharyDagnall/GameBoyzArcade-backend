a644560bb07e0cba9ae2fd0c590ec507efcf2d4b
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-03-09 11:11:25 -0500

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_many :games
+
+    def find_open_game(name_of_the_game)
+        self.games.find{|game| game.name == name_of_the_game && !game.game_over}
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_150515) do
+ActiveRecord::Schema.define(version: 2021_03_09_151641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_150515) do
     t.boolean "game_over"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
